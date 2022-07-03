fc69c89d2982ab787ec9718cfdea719171ba112d
Fazliddin Umirzoqov <fazliddinumirzoqov0122@gmail.com>
2022-07-03 10:48:58 +0500

    learning JavaScript

---
+++ js/script.js
@@ -0,0 +1,47 @@
+// let a = 5;
+// console.log(a);
+
+// function myFunc(a) {
+//   console.log("salom");
+//   return a + 10;
+// }
+// console.log(myFunc(5));
+
+// function func1(c) {
+//   return c + c;
+// }
+// console.log(func1(4));
+
+// let hisobla = (a, b) => {
+//   return a + b;
+// };
+// console.log(hisobla(10, 4));
+
+// function meva(org1, org2, org3) {
+//   console.log("salom" + org1 + org2 + org3);
+// }
+// meva("olma", "olcha", "anor");
+
+// const friends = ["Jonibek", "Shoxrux", "Fazliddin"];
+// console.log(friends.length);
+// friends[1] = "Ibrohim";
+
+// console.log(friends);
+
+// friends.push("Jo`raqo`zi");
+
+// friends.unshift("Begzod");
+// console.log(friends);
+
+// friends.pop();
+
+// console.log(friends);
+
+// friends.splice(0, 1);
+// console.log(friends);
+
+function tugulganYil(a) {
+  let hozirgiYil = 2022;
+  return hozirgiYil - a;
+}
+console.log(tugulganYil(2002));
